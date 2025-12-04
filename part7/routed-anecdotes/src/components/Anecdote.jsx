c3d6544f4d8e8e8129d306a99fef126b18bb12ec
Nyukaa <annashitikova@gmail.com>
2025-12-04 14:19:45 +0200

    same

--- part7/routed-anecdotes/src/components/Anecdote.jsx
+++ part7/routed-anecdotes/src/components/Anecdote.jsx
@@ -3,7 +3,7 @@ const Anecdote = ({ anecdote }) => {
   return (
     <div>
       <h2>
-        {anecdote.content} by {anecdote.auther}
+        {anecdote.content} by {anecdote.author}
       </h2>
       <div>has {anecdote.votes} votes</div>
       <div>
