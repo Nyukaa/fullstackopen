db26dbc04ef9b6a69fe267d9b1829a380581afb8
Nyukaa <annashitikova@gmail.com>
2025-12-28 16:19:32 +0200

    fixed

--- part7_bloglist_7/backend/controllers/blogs.js
+++ part7_bloglist_7/backend/controllers/blogs.js
@@ -5,7 +5,7 @@ const middleware = require("../utils/middleware"); //
 
 console.log("✅ blogsRouter loaded");
 
-// GET: все блоги (работает без токена)
+// GET: all blogs dont need token
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
